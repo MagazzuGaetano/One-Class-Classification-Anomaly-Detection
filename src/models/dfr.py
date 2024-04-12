c895f93ad141f28b34053a63d00b8b6b9fff3e77
lfx <lordfighterx@gmail.com>
2024-04-12 22:52:01 +0200

    feat: add deep learning models

---
+++ src/models/dfr.py
@@ -0,0 +1,106 @@
+from models.feat_cae import FeatCAE
+import torch
+import torch.nn as nn
+from models.feature import Extractor
+from sklearn.decomposition import PCA
+
+
+class DFR(nn.Module):
+    def __init__(self, backbone='vgg19',
+                 cnn_layers=("relu1_1",),
+                 upsample="nearest",
+                 is_agg=True,
+                 kernel_size=(4, 4),
+                 stride=(4, 4),
+                 dilation=1,
+                 featmap_size=(256, 256),
+                 device='cpu',
+                 in_channels=1000,
+                 latent_dim=50,
+                 is_bn=True,
+                 data_loader=None):
+        super(DFR, self).__init__()
+
+        self.backbone = backbone
+        self.cnn_layers = cnn_layers
+        self.upsample = upsample
+        self.is_agg = is_agg
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.dilation = dilation
+        self.featmap_size = featmap_size
+        self.device = device
+
+        self.in_channels = in_channels
+        self.latent_dim = latent_dim
+        self.is_bn = is_bn
+        self.data_loader = data_loader
+
+        # feature extractor
+        self.extractor = Extractor(backbone=self.backbone,
+                cnn_layers=self.cnn_layers,
+                upsample=self.upsample,
+                is_agg=self.is_agg,
+                kernel_size=self.kernel_size,
+                stride=self.stride,
+                dilation=self.dilation,
+                featmap_size=self.featmap_size,
+                device=self.device).to(self.device)
+
+
+        # estimate autoencoder parameters
+        if self.in_channels is None or self.latent_dim is None:
+            if self.data_loader is None:
+                raise Exception('if in_channels or latent_dim are None data_loader must be defined!!!')
+            self.estimate_parameters()
+
+        # autoencoder
+        self.autoencoder = FeatCAE(width=featmap_size[0]//4, height=featmap_size[0]//4, in_channels=self.in_channels,
+                                   latent_dim=self.latent_dim,
+                                   is_bn=self.is_bn).to(self.device)
+
+    def forward(self, x):
+        extracted_feat = self.extractor(x)
+        reconstructed_feat = self.autoencoder(extracted_feat)
+        return extracted_feat, reconstructed_feat
+
+    def get_hotmap(self, x, x_hat):
+        # compute heatmap B x (W // 4) x (H // 4)
+        loss = torch.sum((x - x_hat) ** 2, dim=1)
+
+        # loss = torch.mean((x - x_hat) ** 2, dim=1)
+        return loss
+
+    def loss_fn(self, x, x_hat, dim=None):
+        # B x 2048 x 56 x 56
+        loss = self.get_hotmap(x, x_hat)
+        # B x 56 x 56
+        loss = loss.mean(axis = (1,2)) / (x.shape[0] * self.in_channels)
+        # B x 1
+
+        #loss = torch.mean((x - x_hat) ** 2, dim=dim)
+        return loss
+
+
+    def estimate_parameters(self):
+        print("Estimating one class classifier AE parameter...")
+
+        feats = torch.Tensor()
+        for batch_x, _ in self.data_loader:
+            normal_img = batch_x[0].unsqueeze(0) # first image of the batch
+            normal_img = normal_img.to(self.device)
+            feat = self.extractor.feat_vec(normal_img)
+            feats = torch.cat([feats, feat.cpu()], dim=0)
+        feats = feats.detach().numpy()
+
+        # estimate parameters for ae
+        pca = PCA(n_components=0.8)    # 0.9 here try 0.8
+        pca.fit(feats)
+        n_dim, in_feat = pca.components_.shape
+        print("AE Parameter (in_feat, n_dim): ({}, {})".format(in_feat, n_dim))
+
+        # set ae parameters
+        self.in_channels = in_feat
+        self.latent_dim = n_dim
+
+        
---
+++ src/models/feat_cae.py
@@ -0,0 +1,83 @@
+from models.non_local import NLBlockND
+import torch
+import torch.nn as nn
+
+class FeatCAE(nn.Module):
+    """Autoencoder."""
+
+    def __init__(self, width=64, height=64, in_channels=1000, latent_dim=50, is_bn=True):
+        super(FeatCAE, self).__init__()
+        
+        self.width = width
+        self.height = height
+        self.in_channels = in_channels
+        self.latent_dim = latent_dim
+        self.is_bn = is_bn
+
+        self.relu = nn.ReLU()
+
+        # Encoder
+        self.conv1 = nn.Conv2d(self.in_channels, (self.in_channels + 2 * self.latent_dim) // 2, kernel_size=1, stride=1, padding=0)
+        self.bn_1 = nn.BatchNorm2d(num_features=(self.in_channels + 2 * self.latent_dim) // 2)
+        self.non_local_block1 = NLBlockND(in_channels=(self.in_channels + 2 * self.latent_dim) // 2, mode='embedded', dimension=2, bn_layer=True)
+
+        self.conv2 = nn.Conv2d((self.in_channels + 2 * self.latent_dim) // 2, 2 * self.latent_dim, kernel_size=1, stride=1, padding=0)
+        self.bn_2 = nn.BatchNorm2d(num_features=2 * self.latent_dim)
+        self.non_local_block2 = NLBlockND(in_channels=2 * self.latent_dim, mode='embedded', dimension=2, bn_layer=True)
+
+        # Embedding
+        self.conv3 = nn.Conv2d(2 * self.latent_dim, self.latent_dim, kernel_size=1, stride=1, padding=0)
+
+        # Decoder 1x1 conv to reconstruct the rgb values
+        self.conv4 = nn.Conv2d(latent_dim, 2 * latent_dim, kernel_size=1, stride=1, padding=0)
+        self.bn_4 = nn.BatchNorm2d(num_features=2 * latent_dim)
+        self.non_local_block3 = NLBlockND(in_channels=2 * self.latent_dim, mode='embedded', dimension=2, bn_layer=True)
+
+        self.conv5 = nn.Conv2d(2 * latent_dim, (in_channels + 2 * latent_dim) // 2, kernel_size=1, stride=1, padding=0)
+        self.bn_5 = nn.BatchNorm2d(num_features=(in_channels + 2 * latent_dim) // 2)
+        self.non_local_block4 = NLBlockND(in_channels=(in_channels + 2 * latent_dim) // 2, mode='embedded', dimension=2, bn_layer=True)
+
+        self.conv6 = nn.Conv2d((in_channels + 2 * latent_dim) // 2, in_channels, kernel_size=1, stride=1, padding=0)
+
+
+    def forward(self, x):
+        # encoder
+        x = self.relu(self.bn_1(self.conv1(x)))
+        x = self.non_local_block1(x)
+
+        x = self.relu(self.bn_2(self.conv2(x)))
+        x = self.non_local_block2(x)
+
+        x = self.conv3(x)
+
+        # decoder
+        x = self.relu(self.bn_4(self.conv4(x)))
+        x = self.non_local_block3(x)
+
+        x = self.relu(self.bn_5(self.conv5(x)))
+        x = self.non_local_block4(x)
+
+        x = self.conv6(x)
+
+        return x
+
+if __name__ == "__main__":
+    import numpy as np
+    import time
+
+    from torchsummary import summary
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    x = torch.Tensor(np.random.randn(1, 2048, 56, 56)).to(device)
+    feat_ae = FeatCAE(width=56, height=56, in_channels=2048, latent_dim=148).to(device)
+
+    summary(feat_ae, (2048, 56, 56), 1, device)
+
+    time_s = time.time()
+    for i in range(10):
+        time_ss = time.time()
+        out = feat_ae(x)
+        print("Time cost:", (time.time() - time_ss), "s")
+
+    print("Time cost:", (time.time() - time_s), "s")
+    print("Feature (n_samples, n_features):", out.shape)
